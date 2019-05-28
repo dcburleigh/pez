a507a5da9d60348e5c7a306c69e989a87117754b
Darin Burleigh <9852399+dcburleigh@users.noreply.github.com>
2019-05-28 14:46:48 -0500

    deprecated, moved to spark/auth.py

--- server/auth/__init__.py
+++ server/auth/__init__.py
@@ -2,9 +2,16 @@ import os
 import re
 from webexteamssdk import WebexTeamsAPI
 
+# NOT USED
+
 auth_token = None
 auth_token_admin = None
-webex_teams_access_token = None
+webex_teams_access_token = None  # bot
+org_id = None
+
+t = os.environ.get('ORG_ID')
+if t:
+    org_id = t
 
 t = os.environ.get('PEZ_AUTH_TOKEN')
 if t:
@@ -23,6 +30,17 @@ t = os.environ.get('WXT_ACCESS_TOKEN')
 if t:
     webex_teams_access_token = t
     api = WebexTeamsAPI(access_token=webex_teams_access_token )
+
+def check_org_id(org_id_in):
+    if not org_id:
+        print("no ORG ")
+        return False
+
+    if org_id == org_id_in:
+        return True
+    else:
+        print("%s <> %s" % ( org_id, org_id_in))
+        return False
 
 def check_auth(h=None, token=None):
 
