9a53b9a368ed22808c382c70264de5d78aa136ea
Darin Burleigh <9852399+dcburleigh@users.noreply.github.com>
2019-05-28 14:49:53 -0500

    path: /directory - hook for bot

--- server/main.py
+++ server/main.py
@@ -12,25 +12,27 @@ GET /hello
 """
 
 from flask import Flask
-from flask import request, abort, Response, send_from_directory
+from flask import request, abort, Response
+#, send_from_directory
 import json
 #import sys
 import re
-import os
-from auth import check_auth, api
+#import os
+from spark.auth import check_auth
 from deldevdb.staffdb import StaffDB
-
+from spark.hook_message import HookMessage
+from spark.hook_handler import pez_handler
 
 app = Flask(__name__)
-dbh = None
+#dbh = None
 last_err = None
-staff_dbh = None
 staff_dbh = StaffDB()
+help_pattern = re.compile('.*help.*')
+whois_pattern = re.compile('whois (\w+) ')
 
 @app.route("/")
 def about(info=''):
-    return "PezBot " + info
-
+    return "PezBot Directory " + info
 
 @app.errorhandler(401)
 def not_authorized(error):
@@ -48,12 +50,14 @@ def page_not_found(error):
 
 @app.route("/test", methods=['GET', 'POST'])
 def test():
+    info = {}
     err = check_auth(request.headers)
     if err:
-        abort(401, err)
+        #abort(401, err)
+        info['error'] =  err
 
     text = ''
-    info  = { 'method': request.method}
+    info['method'] =  request.method
 
     #print request.headers
     print("got here method={}".format( request.method))
@@ -68,14 +72,62 @@ def test():
     text = json.dumps( info, indent=4)
     return text
 
+
+@app.route("/directory", methods=[ 'POST'])
+def directory():
+    info = {}
+
+    err = check_auth(request.headers)
+    if err:
+        abort(401, err)
+
+    #
+    # parse hook
+    #
+    if not request.is_json:
+        abort(400,  "not json")
+        #abort( {'400': "not json"})
+        info['error'] = "not json"
+        return json.dumps( info, indent=4)
+
+    try:
+        obj = request.get_json()
+    except Exception as err:
+        #print err
+        info['data_in'] = request.data
+        info['error'] = "no json object" + str(err)
+        return json.dumps( info, indent=4)
+
+    if not obj:
+        info['error'] = "no json object"
+        return json.dumps( info, indent=4)
+
+    #print("got message id=" + obj['id'])
+
+    msg = HookMessage( obj )
+    msg.get_text()
+    msg.get_creator()
+    #msg.show()  # TESTING 
+
+    if msg.from_me():
+        # don't respond to my own posts
+        return
+
+    if not msg.check_org():
+        #abort('401': "invalid org")
+        abort('401', "invalid org")
+        return
+
+    return pez_handler(msg)
+
 @app.route("/whois", methods=['GET', 'POST'])
 def whois():
     info = {}
-
-    err = check_auth(h=request.headers)
+    err = check_auth(request.headers)
     if err:
-        info['error'] = err
-        return json.dumps( info, indent=4)
+        abort('401', err)
+        return
+
     if request.method == 'POST':
 
         #info['header'] = request.headers
@@ -86,7 +138,7 @@ def whois():
         if not request.is_json:
             info['error'] = "not json"
             #abort(400)
-            #abort( {'400': "not json"})
+            abort( {'400', "not json"})
             return json.dumps( info, indent=4)
         try:
             obj = request.get_json()
